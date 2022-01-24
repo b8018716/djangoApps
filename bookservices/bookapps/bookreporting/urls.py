82a8cee4ed7353ffba486ba18a1a48953684a8d1
b8018716 <b8018716@shu.ac.uk>
2022-01-24 15:28:55 +0000

    first section of S2 L2

--- bookservices/bookapps/bookreporting/urls.py
+++ bookservices/bookapps/bookreporting/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from users import views as user_views
 
 from . import views
 
 urlpatterns =[
 
     path('',views.home, name='bookreporting-home'),
-    path('about/',views.about, name='bookreporting-about')
+    path('about/',views.about, name='bookreporting-about'),
+    path('register/', user_views.register, name='register'),
 ]
