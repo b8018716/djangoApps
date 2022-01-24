f44c35314bbf31b3983f708a4659439e0dcd1159
b8018716 <b8018716@shu.ac.uk>
2022-01-24 15:41:54 +0000

    as above

--- bookservices/bookapps/users/views.py
+++ bookservices/bookapps/users/views.py
@@ -12,6 +12,9 @@ def register(request):
             username=form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
             return redirect('bookreporting-home')
+            
+        else: return redirect('bookreporting-about')
+
     else:
         form = UserCreationForm()
         return render(request, 'users/register.html', {'form':form})
